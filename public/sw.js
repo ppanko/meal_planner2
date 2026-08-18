d9e6d736e4f59ae853ba3a0358fd4b1ec9117a9c
ppanko <18711790+ppanko@users.noreply.github.com>
2026-08-18 15:00:27 -0400

    increment version

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meal-planner-v1'
+const CACHE = 'meal-planner-v2'
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.add('./')).then(() => self.skipWaiting()))
